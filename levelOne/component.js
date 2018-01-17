7635067666ec01facc700a1ce36d03d2ce449a39
Chris <c.lamothe25@gmail.com>
2018-01-16 20:58:44 -0500

    initial commit

---
+++ levelOne/component.js
@@ -0,0 +1,5 @@
+class RobotBox extends React.Component {
+    render () {
+      return ( <div>Hello From React</div> )
+    }
+  }
---
+++ levelOne/jsxAndPlain.js
@@ -0,0 +1,11 @@
+class RobotTime extends React.Component {
+    render() {
+      const pi = Math.PI;
+      return (
+        <div className="is-tasty-pie">
+          The value of PI is approximately {pi}
+        </div>
+      );
+    }
+  }
+   
